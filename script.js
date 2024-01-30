ed8d21b27ff9238afe492646469d9abd8e9afc63
R-level <47988410+R-level@users.noreply.github.com>
2024-01-30 13:50:16 +0200

    Changes
    Added animations

---
+++ script.js
@@ -0,0 +1,42 @@
+const painImages = document.querySelectorAll('.painImage');
+const achingImages = document.querySelectorAll('.achingImage');
+const stigmaImages = document.querySelectorAll('.stigmaImage');
+const isElementInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const handleScroll = () => {
+    achingImages.forEach((image)=>{
+        if(isElementInViewport(image)){
+            image.classList.add('jiggle');
+        }else{
+            image.classList.remove('jiggle')
+        }
+      });
+  painImages.forEach((image) => {
+    if (isElementInViewport(image)) {
+      image.classList.add('wobble');
+    } else {
+      image.classList.remove('wobble');
+    }
+  });
+  stigmaImages.forEach((image)=>{
+    if(isElementInViewport(image)){
+        image.classList.add('shrink');}
+        else{
+            image.classList.remove('shrink');
+        }
+    }
+  );
+
+};
+
+window.addEventListener('scroll', handleScroll);
+window.addEventListener('resize', handleScroll);
+handleScroll();
