4bc0cf76b0ca6e2b799815047d021bf1e15b0913
R-level <47988410+R-level@users.noreply.github.com>
2024-03-06 13:25:38 +0200

    Update to Menstrual graph
    Test classes made for finding suitable interactivity.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const painImages = document.querySelectorAll('.painImage');
 const achingImages = document.querySelectorAll('.achingImage');
 const stigmaImages = document.querySelectorAll('.stigmaImage');
-
+const menstrualGraph = document.getElementById('#menstrual_graph');
+//const graphSlices = menstrualGraph.querySelectorAll('span');
 
 const isElementInViewport = (el) => {
   const rect = el.getBoundingClientRect();
@@ -41,4 +42,35 @@ const handleScroll = () => {
 
 window.addEventListener('scroll', handleScroll);
 window.addEventListener('resize', handleScroll);
+
+const createGraphInteraction = () => {
+
+ /* graphSlices.forEach((slice)=>{
+    slice.addEventListener('click',()=>{
+      const newParagraph = document.createElement('p');
+      if(slice.id==="menses_arc"){
+        newParagraph.textContent = 'The menses phase: This phase begins on the first day of your period. It\'s when the lining of your uterus sheds through your vagina if pregnancy hasn\'t occurred. Most people bleed for three to five days, but a period lasting only three days to as many as seven days is usually not a cause for worry.';
+        menstrualSection.append(newParagraph);
+      }
+      else if(slice.id==="follicular_arc"){
+
+      }
+      else if(slice.id==="ovulation_arc"){
+
+      }
+      else if(slice.id==="luteal_arc"){
+
+      }
+
+    })
+  })*/
+  const button = document.querySelector('press');
+  button.addEventListener('click',()=>{
+   
+      
+       
+      button.textContent='The menses phase';
+  }
+)  };
+createGraphInteraction();
 handleScroll();
