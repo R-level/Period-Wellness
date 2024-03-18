333dd30289f3e4a56234271f8ef05311ced39cdd
R-level <47988410+R-level@users.noreply.github.com>
2024-03-18 12:25:34 +0200

    update functionality in graph

--- script.js
+++ script.js
@@ -50,35 +50,38 @@ const createGraphInteraction = () => {
     graphSlices.forEach((slice)=>{
     slice.addEventListener('click',()=>{
       const newParagraph = document.createElement('p');
+    
+     
+
+      const oldParagraph = menstrualSection.querySelector('p');
+     
+      if(slice.id==="menses_arc"){
+        newParagraph.textContent = 'The menses phase: This phase begins on the first day of your period. It\'s when the lining of your uterus sheds through your vagina if pregnancy hasn\'t occurred. Most people bleed for three to five days, but a period lasting only three days to as many as seven days is usually not a cause for worry.';
+        
+      }
+      else if(slice.id==="follicular_arc"){
+        newParagraph.textContent ='The follicular phase: This phase begins on the day you get your period and ends at ovulation (it overlaps with the menses phase and ends when you ovulate). During this time, the level of the hormone estrogen rises, which causes the lining of your uterus (the endometrium) to grow and thicken. In addition, another hormone — follicle-stimulating hormone (FSH) — causes follicles in your ovaries to grow. During days 10 to 14, one of the developing follicles will form a fully mature egg (ovum).';
+      }
+      else if(slice.id==="ovulation_arc"){
+        newParagraph.textContent ='Ovulation: This phase occurs roughly at about day 14 in a 28-day menstrual cycle. A sudden increase in another hormone— luteinizing hormone (LH) — causes your ovary to release its egg. This event is ovulation.'
+        
+      }else if(slice.id==="luteal_arc"){
+        newParagraph.textContent= 'The luteal phase: This phase lasts from about day 15 to day 28. Your egg leaves your ovary and begins to travel through your fallopian tubes to your uterus. The level of the hormone progesterone rises to help prepare your uterine lining for pregnancy. If the egg becomes fertilized by sperm and attaches itself to your uterine wall (implantation), you become pregnant. If pregnancy doesn’t occur, estrogen and progesterone levels drop and the thick lining of your uterus sheds during your period.'
+        
+      }
       
-      const oldParagraph = menstrualSection.querySelector('p')
       if(oldParagraph){
+        oldParagraph.classList.remove('fade-in');
         oldParagraph.classList.add('fade-out');
         setTimeout(()=>{
           menstrualSection.removeChild(oldParagraph);
         }, 500);
       }
-      if(slice.id==="menses_arc"){
-        newParagraph.textContent = 'The menses phase: This phase begins on the first day of your period. It\'s when the lining of your uterus sheds through your vagina if pregnancy hasn\'t occurred. Most people bleed for three to five days, but a period lasting only three days to as many as seven days is usually not a cause for worry.';
-        menstrualSection.append(newParagraph);
-      }
-      else if(slice.id==="follicular_arc"){
-        newParagraph.textContent ='The follicular phase: This phase begins on the day you get your period and ends at ovulation (it overlaps with the menses phase and ends when you ovulate). During this time, the level of the hormone estrogen rises, which causes the lining of your uterus (the endometrium) to grow and thicken. In addition, another hormone — follicle-stimulating hormone (FSH) — causes follicles in your ovaries to grow. During days 10 to 14, one of the developing follicles will form a fully mature egg (ovum)';
-        menstrualSection.append(newParagraph);
-      }
-      else if(slice.id==="ovulation_arc"){
-        newParagraph.textContent ='Ovulation: This phase occurs roughly at about day 14 in a 28-day menstrual cycle. A sudden increase in another hormone— luteinizing hormone (LH) — causes your ovary to release its egg. This event is ovulation.'
-        menstrualSection.append(newParagraph);
-      }
-      else if(slice.id==="luteal_arc"){
-        newParagraph.textContent= 'The luteal phase: This phase lasts from about day 15 to day 28. Your egg leaves your ovary and begins to travel through your fallopian tubes to your uterus. The level of the hormone progesterone rises to help prepare your uterine lining for pregnancy. If the egg becomes fertilized by sperm and attaches itself to your uterine wall (implantation), you become pregnant. If pregnancy doesn’t occur, estrogen and progesterone levels drop and the thick lining of your uterus sheds during your period.'
-        menstrualSection.append(newParagraph);
-      }
+
       newParagraph.classList.add('fade-in');
-    })
-  })
-  
-  };
+      setTimeout(()=>{menstrualSection.append(newParagraph);},500);
+      
+    })})};
 
 
 createGraphInteraction();
