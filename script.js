1119399367f55258cf69a7afed2a63e849d7ef1f
R-level <47988410+R-level@users.noreply.github.com>
2024-06-28 10:00:22 +0200

    Update to graph and removal of Debunking section
    Updated graph interaction and removed Debunking section

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const painImage = document.querySelector('.painImage');
 const achingImage = document.querySelector('.achingImage');
 const stigmaImage = document.querySelector('.stigmaImage');
 const menstrualGraph = document.querySelector('#menstrual_graph');
-const menstrualSection = document.querySelector('#MenstrualCycle div.ColumnsLeft');
+const menstrualSection = document.querySelector('.MenstrualCycle div.ColumnsLeft');
 const graphSlices = menstrualGraph.querySelectorAll('span');
-const menstrualPage = document.querySelector('#MenstrualCycle');
+const menstrualPage = document.querySelector('.MenstrualCycle');
 const infoCard = document.querySelector(".info_card");
 const infoCardType = document.querySelector(".info_card #type");
 const infoCardText = document.querySelector(".info_card #text");
 const infoCardMessage = document.querySelector(".info_card #message");
-const DebunkingSection = document.getElementById("Debunking");
+
 let infoType = "";
 // Add event listeners to the images
 achingImage.addEventListener('click', ()=>{
@@ -138,216 +138,16 @@ const createGraphInteraction = () => {
       
      resetGraph();
   }
- class Info{
- constructor(type, text, message){
-     this.type = type;
-     this.text = text;
-     this.message = message;
-   }
-   getType(){
-    return this.type;
-   }
-   getText(){
-    return this.text;
-   }
-   getMessage(){
-     return this.message;
-   }
- }
+ 
 
- const infoArray = [];
+
 /**
  * This function will get the JSON data that contains Period Myth and Fact information.
  * The data will be collected and processed into an array
  */
-async function getData(){
-  const response = await fetch('https://raw.githubusercontent.com/R-level/Period-Wellness/main/data/info.json');
-  const data = await response.json();
-  const infoObj = JSON.stringify(data);
-  const infoList = JSON.parse(infoObj);
-const list = infoList.info;
-console.log(infoList);
-   for(const info of list)
-   {
-    
-infoArray.push(new Info(info.type, info.text, info.message));
-   }
-  
-let paragraphInfo = document.createElement("p");
-
-paragraphInfo.style.font="1.5em 'Roboto', sans-serif";
-paragraphInfo.style.color="#f1ecec";
-const info = infoArray[0]
-
-//paragraphInfo.innerText=info.getText();
-infoCard.appendChild(paragraphInfo);
-
-};
 
 
 
-
-/** Handles the click event for the info card 
- * Changes the color of the card and changes the info in the card
-*/
-let i = 0; //iterator for infoArray
-infoCard.addEventListener("click", ()=>{
-
- infoCard.style.color="red";
-  let info = infoArray[Math.floor(Math.random()*(infoArray.length-1))];
-  if(i<infoArray.length){
-  info=infoArray[i]
-  i++;
-  }
-  else{
-    i=0;
-  }
-   infoType = info.getType();
- 
-  let paragraphInfo = document.createElement("p");
-  paragraphInfo.innerText=info.getText();
-  paragraphInfo.style.font="1.5em 'Roboto', sans-serif";
-  paragraphInfo.style.color="#f1ecec";
-  infoCard.innerHTML="";
- if(infoType==="myth"){
-  if(infoCard.classList.contains("info_fact")){
-    infoCard.classList.remove("info_fact");}
-    infoCard.classList.add("info_myth");
-    //fade out old paragraph
-    infoCard.appendChild(paragraphInfo); 
-  } if(infoType==="fact"){
-    if(infoCard.classList.contains("info_myth")){
-    infoCard.classList.remove("info_myth");}
-    infoCard.classList.add("info_fact")
-    infoCard.appendChild(paragraphInfo); 
-  }});
-
-  const info_btns = document.querySelectorAll(".info_btn");
-  const floatingMessage = document.createElement("p");
- 
-  info_btns.forEach( (btn)=>{
-    btn.addEventListener("click", ()=>{
-      console.log(infoType);
-   if(infoType!=""){
-   
-    //Check if the user chose correctly
-    if(btn.TextContent==infoType){
-      floatingMessage.textContent="True";
-      DebunkingSection.append=floatingMessage;
-      infoType="";
-    }
-    else{
-      floatingMessage.textContent="False";
-      DebunkingSection.append=floatingMessage;
-      infoType="";
-    }
-   }
-    })
-  })
 createGraphInteraction();
-getData();
-
-/** TEST for card javascript 
-'use strict';*/
-
-'use strict';
-
-var tinderContainer = document.querySelector('.tinder');
-var allCards = document.querySelectorAll('.tinder--card');
-var nope = document.getElementById('nope');
-var love = document.getElementById('love');
-
-function initCards(card, index) {
-  var newCards = document.querySelectorAll('.tinder--card:not(.removed)');
-
-  newCards.forEach(function (card, index) {
-    card.style.zIndex = allCards.length - index;
-    card.style.transform = 'scale(' + (20 - index) / 20 + ') translateY(-' + 30 * index + 'px)';
-    card.style.opacity = (10 - index) / 10;
-  });
-  
-  tinderContainer.classList.add('loaded');
-}
-
-initCards();
-
-allCards.forEach(function (el) {
-  var hammertime = new Hammer(el);
-
-  hammertime.on('pan', function (event) {
-    el.classList.add('moving');
-  });
-
-  hammertime.on('pan', function (event) {
-    if (event.deltaX === 0) return;
-    if (event.center.x === 0 && event.center.y === 0) return;
-
-    tinderContainer.classList.toggle('tinder_love', event.deltaX > 0);
-    tinderContainer.classList.toggle('tinder_nope', event.deltaX < 0);
-
-    var xMulti = event.deltaX * 0.03;
-    var yMulti = event.deltaY / 80;
-    var rotate = xMulti * yMulti;
-
-    event.target.style.transform = 'translate(' + event.deltaX + 'px, ' + event.deltaY + 'px) rotate(' + rotate + 'deg)';
-  });
-
-  hammertime.on('panend', function (event) {
-    el.classList.remove('moving');
-    tinderContainer.classList.remove('tinder_love');
-    tinderContainer.classList.remove('tinder_nope');
-
-    var moveOutWidth = document.body.clientWidth;
-    var keep = Math.abs(event.deltaX) < 80 || Math.abs(event.velocityX) < 0.5;
-
-    event.target.classList.toggle('removed', !keep);
-
-    if (keep) {
-      event.target.style.transform = '';
-    } else {
-      var endX = Math.max(Math.abs(event.velocityX) * moveOutWidth, moveOutWidth);
-      var toX = event.deltaX > 0 ? endX : -endX;
-      var endY = Math.abs(event.velocityY) * moveOutWidth;
-      var toY = event.deltaY > 0 ? endY : -endY;
-      var xMulti = event.deltaX * 0.03;
-      var yMulti = event.deltaY / 80;
-      var rotate = xMulti * yMulti;
-
-      event.target.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + rotate + 'deg)';
-      initCards();
-    }
-  });
-});
-
-function createButtonListener(love) {
-  return function (event) {
-    var cards = document.querySelectorAll('.tinder--card:not(.removed)');
-    var moveOutWidth = document.body.clientWidth * 1.5;
-
-    if (!cards.length) return false;
-
-    var card = cards[0];
-
-    card.classList.add('removed');
-
-    if (love) {
-      card.style.transform = 'translate(' + moveOutWidth + 'px, -100px) rotate(-30deg)';
-    } else {
-      card.style.transform = 'translate(-' + moveOutWidth + 'px, -100px) rotate(30deg)';
-    }
-
-    initCards();
-
-    event.preventDefault();
-  };
-}
-
-var nopeListener = createButtonListener(false);
-var loveListener = createButtonListener(true);
-console.log(loveListener);
-
-nope.addEventListener('click', nopeListener);
-love.addEventListener('click', loveListener);
 
 
-/**End of test */
